d85050aadf3248cc28c0d5d772830c0b319fa915
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-13 23:42:41 -0300

    ex02 - Casting and date processing

---
+++ curso_programacao/src/curso_programacao/ex002.java
@@ -0,0 +1,43 @@
+package curso_programacao;
+
+public class ex002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float b, B, h, area;
+		
+		b = 6f;
+		B = 8f;
+		h = 5f;
+		
+		area = (b + B) / 2f * h;
+		
+		
+		System.out.println(area);
+		
+		//
+		
+		int x, y;
+		double resultado;
+		
+		x = 5;
+		y = 2;
+		
+		resultado = (double) x / y; //****Casting****
+		
+		System.out.println(resultado);
+		
+		//
+		
+		double w;
+		int z;
+		
+		w = 5.0;
+		z = (int) w;
+		
+		System.out.println(w);
+		System.out.println(z);
+	}
+
+}
