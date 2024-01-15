c7b55297329d74e43ae144d2f9156fe50a18a840
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-14 22:16:06 -0300

    ex005 - finished

--- curso_programacao/src/curso_programacao/ex005.java
+++ curso_programacao/src/curso_programacao/ex005.java
@@ -14,11 +14,22 @@ public class ex005 {
 		B = Math.sqrt(y);
 		C = Math.sqrt(25.0);
 		
-		System.out.printf("Raiz quadrada de %d%n", x);
-		System.out.printf("Raiz quadrada de %d%n", y);
-		System.out.printf("Raiz quadrada de %d%n", z);
+		System.out.printf("Raiz quadrada de %.1f = %.16f%n", x, A);
+		System.out.printf("Raiz quadrada de %.1f = %.1f%n", y, B);
+		System.out.printf("Raiz quadrada de 25 = %.1f%n", C);
 		
-		A = Math.pow(A, B);
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		
+		System.out.printf("%.1f elevado a %.1f = %.1f%n", x, y, A);
+		System.out.printf("%.1f elevado ao quadrado = %.1f%n", x, B);
+		System.out.printf("5 elevado ao quadrado = %.1f%n", C);
+		
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.printf("Valor absoluto de %.1f = %.1f%n", y, A);
+		System.out.printf("Valor absoluto de %.1f = %.1f%n", z, B);
 	}
 
 }
