5e22d253a31f1b59fb06e04368ec30180d55e8cb
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-15 15:20:01 -0300

    ex008 - Java exercise(2)

---
+++ curso_programacao/src/_01_sequentialStructure/ex008.java
@@ -0,0 +1,25 @@
+package _01_sequentialStructure;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double raio, area;
+		
+		System.out.println("Digite o valor do raio:");
+		raio = sc.nextDouble();
+		area = 3.14159 * Math.pow(raio, 2.0);
+		
+		System.out.printf("Resultado da área: %.4f%n", area);
+		
+		sc.close();
+	}
+
+}
