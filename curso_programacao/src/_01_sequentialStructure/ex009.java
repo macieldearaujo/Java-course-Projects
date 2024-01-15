7e6a446662a714eb2ec0400e3adbd2796d1eb435
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-15 15:29:55 -0300

    ex009 - Java exercise(3)

---
+++ curso_programacao/src/_01_sequentialStructure/ex009.java
@@ -0,0 +1,28 @@
+package _01_sequentialStructure;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex009 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b, c, d, diferenca;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		d = sc.nextInt();
+		
+		diferenca = (a * b - c * d);
+		
+		System.out.println("Diferença = " + diferenca);
+		
+		sc.close();
+	}
+
+}
