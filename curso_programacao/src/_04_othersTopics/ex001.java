fa31b31eb1b73b5ca649ff32a1154b397a571f09
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-17 19:07:02 -0300

    ex001 - bitwise operator

---
+++ curso_programacao/src/_04_othersTopics/ex001.java
@@ -0,0 +1,23 @@
+package _04_othersTopics;
+
+import java.util.Scanner;
+
+public class ex001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int c = sc.nextInt();
+		int mask = 0b100000;
+		
+		if((c & mask) != 0) {
+			System.out.println("The 6th bit is true!");
+		} else {
+			System.out.println("The 6th bit is false!");
+		}
+		
+	}
+
+}
