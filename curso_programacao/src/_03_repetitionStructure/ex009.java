ec1631a3c8ccb297341140c0abc1bf1b936e9b67
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-17 00:05:57 -0300

    ex009 - for exercises - 3

---
+++ curso_programacao/src/_03_repetitionStructure/ex009.java
@@ -0,0 +1,28 @@
+package _03_repetitionStructure;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex009 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um valor de repetições:");
+		int quant = sc.nextInt();
+		
+		System.out.println("Digite " + quant + " valores abaixo:");
+		double v1 = sc.nextDouble();
+		double v2 = sc.nextDouble();
+		double v3 = sc.nextDouble();
+		
+		double media = (v1*2 + v2*3 + v3*5) / (2 + 3 + 5);
+		
+		System.out.printf("%.1f", media);
+		
+		sc.close();
+	}
+
+}
