f95534646a24fe1b37caf3369235a5248c3b4b8a
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-18 18:17:22 -0300

    ex004 - object oriented - 2

---
+++ curso_programacao/src/_05_objectOriented/ex004.java
@@ -0,0 +1,44 @@
+package _05_objectOriented;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class ex004 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Employee employee = new Employee();
+		
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		employee.name = name;
+		
+		System.out.print("Gross salary: ");
+		double grossSalary = sc.nextDouble();
+		employee.grossSalary = grossSalary;
+		
+		System.out.print("Tax: ");
+		double tax = sc.nextDouble();
+		employee.tax = tax;
+		
+		System.out.println();
+		System.out.println("Employee: " + employee);
+		
+		System.out.println();
+		System.out.print("Which percentage to increasy salary? ");
+		double percentage = sc.nextDouble();
+		employee.increasePercentage(percentage);
+		
+		System.out.println();
+		System.out.println("Update data: " + employee);
+		
+		sc.close();
+	}
+
+}
