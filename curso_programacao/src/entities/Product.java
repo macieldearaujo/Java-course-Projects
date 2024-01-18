b4762556aa896da011157e82208b222ccad93e85
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-18 17:17:19 -0300

    add a toString method to return the description of the object

--- curso_programacao/src/entities/Product.java
+++ curso_programacao/src/entities/Product.java
@@ -18,4 +18,12 @@ public class Product {
 		this.quantity -= quantity;
 	}
 	
+	public String toString() {
+		return name 
+				+ ", $ " 
+				+ String.format("%.2f", price)
+				+ ", " + quantity 
+				+ " units, Total: $ " 
+				+ String.format("%.2f", totalValueinStock());
+	}
 }
