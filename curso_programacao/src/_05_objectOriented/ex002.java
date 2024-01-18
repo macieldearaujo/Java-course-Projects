68209e9bb221b917ba9f2c2d535c64bf2004e1eb
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-18 15:47:09 -0300

    ex001 - triangle class

---
+++ curso_programacao/src/_05_objectOriented/ex002.java
@@ -0,0 +1,34 @@
+package _05_objectOriented;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class ex002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		
+		System.out.println("Enter product data:");
+		 
+		System.out.print("Name:  ");
+		product.name = sc.nextLine();
+		
+		System.out.print("Price:  ");
+		product.price = sc.nextDouble();
+		
+		System.out.print("Quantity in stock:  ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		
+		sc.close();
+	}
+	
+}
