b863309a134b8f38c6376875a61db81ef17c93ef
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-18 14:51:51 -0300

    created a Program to set the triangle

---
+++ curso_programacao/src/_05_objectOriented/Program.java
@@ -0,0 +1,46 @@
+package _05_objectOriented;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangle;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+		
+		double areaX, areaY;
+		
+		System.out.println("Enter the measures of triangle X:");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		areaX = x.calculateArea();
+		
+		System.out.println("Enter the measures of triangle Y:");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		areaY = y.calculateArea();
+		
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		
+		char large = (areaX > areaY) ? 'X' : 'Y';
+				
+		System.out.println("Large area: " + large);
+		
+		sc.close();
+	}
+	
+}
