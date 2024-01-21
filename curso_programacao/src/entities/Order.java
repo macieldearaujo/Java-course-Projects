66529298600a01cf4dd6a1cbc4584212f2bee1eb
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-21 01:11:56 -0300

    ex03 - acess modifier

---
+++ curso_programacao/src/entities/Order.java
@@ -0,0 +1,34 @@
+package entities;
+
+import java.util.Date;
+
+public class Order {
+
+	/* private: o membro só pode ser acessado na própria classe;
+	(nada): o membro só pode ser acessado nas classes do mesmo pacote;
+	protected: o membro só pode ser acessado no mesmo pacote, bem como em subclasses de pacotes diferentes;
+	public: o membro é acessado em todas as classes, a menos que seja em módulo diferente.
+	*/
+	
+	private Date date;
+	private Product product;
+	
+	public Order(Date date, Product product) {
+		this.date = date;
+		this.product = product;
+		this.product.name = "TV";
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+}
