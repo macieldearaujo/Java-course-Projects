d82224f93e412ca6f721b7572bbc00a592bb5bd7
douglasmcl <douglasmacielaraujo@outlook.com>
2024-01-22 20:02:58 -0300

    ex006 - array exercise

---
+++ curso_programacao/src/_07_arrays_lists_memory/ex006_array.java
@@ -0,0 +1,45 @@
+package _07_arrays_lists_memory;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex006_array {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("How many values will each vector have? ");
+		int n = sc.nextInt();
+		int[] vectorA = new int[n];
+		int[] vectorB = new int[n];
+		int[] vectorC = new int[n];
+
+		System.out.println("Type the values of vector A:");
+
+		for (int i = 0; i < n; i++) {
+			vectorA[i] = sc.nextInt();;
+		}
+		
+		System.out.println("Type the values of vector B:");
+
+		for (int i = 0; i < n; i++) {
+			vectorB[i] = sc.nextInt();;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			vectorC[i] = vectorA[i] + vectorB[i];
+		}
+		
+		System.out.println("RESULTING VECTOR:");
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println(vectorC[i]);
+		}
+		
+		
+
+		sc.close();
+	}
+
+}
