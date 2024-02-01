198010ccb61fd8101f280ea8bb6dcf1fe75f20e6
douglasmcl <douglasmacielaraujo@outlook.com>
2024-02-01 18:35:58 -0300

    program1 - enum

---
+++ curso_programacao/src/_09_enum/Program_1.java
@@ -0,0 +1,24 @@
+package _09_enum;
+
+import java.util.Date;
+import java.util.Scanner;
+
+import entities.OrderProduct;
+import entities.enums.OrderStatus;
+
+public class Program_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		OrderProduct product = new OrderProduct(1080, new Date(), OrderStatus.PROCESSING);
+		System.out.println(product);
+		
+		OrderStatus os1 = OrderStatus.PENDING_PAYMENT;
+		 OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		 
+		 System.out.println(os1);
+		 System.out.println(os2);
+	}
+
+}
