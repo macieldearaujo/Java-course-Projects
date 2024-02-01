787cf94beddbf3ad8c0cd243322c3656507dc993
douglasmcl <douglasmacielaraujo@outlook.com>
2024-02-01 18:36:16 -0300

    program1 - enum (class)

---
+++ curso_programacao/src/entities/OrderProduct.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.util.Date;
+
+import entities.enums.OrderStatus;
+
+public class OrderProduct {
+
+	private Integer id;
+	private Date moment;
+	private OrderStatus status;
+	
+	public OrderProduct() {
+	}
+
+	public OrderProduct(Integer id, Date moment, OrderStatus status) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderProduct [id=" + id + ", moment=" + moment + ", status=" + status + "]";
+	}
+	
+	
+}
