c656a3d50c0c6046007d208a45a1aa9f2b5f8658
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-06 21:39:29 -0300

    program3 - stringBuilder and enum solved exercise - 2

---
+++ curso_programacao/src/_09_enum/Program_3.java
@@ -0,0 +1,27 @@
+package _09_enum;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import entities.Comment;
+import entities.Post;
+
+public class Program_3 {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+		
+		Post p1 = new Post(sdf.parse("21/06/2018 13:05:18"), "Travelling to New Zealand", "I'm going to visit this wonderful country!", 12);
+		p1.addComment(new Comment("Have a nice trip!"));
+		p1.addComment(new Comment("How that's awesome!"));
+		
+		Post p2 = new Post(sdf.parse("26/07/2018 23:14:19"), "Good night guys", "See you tomorrow", 5);
+		p2.addComment(new Comment("Good night!"));
+		p2.addComment(new Comment("May the Force be with you!"));
+		
+		System.out.println(p1);
+		System.out.println(p2);
+	}
+}
