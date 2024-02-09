90debf726868012d0d1bdca2ba7652d7c6636f21
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-08 23:28:10 -0300

    program1 - inheritance (subclass)

--- curso_programacao/src/entities/BussinessAccount.java
+++ curso_programacao/src/entities/BussinessAccount.java
@@ -12,6 +12,18 @@ public class BussinessAccount extends Account {
 		super(account, holder, balance);
 		this.loanLimit = loanLimit;
 	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
 	
-	
+	public void loan(Double amount) {
+		if(amount <= loanLimit) {
+			balance += amount - 10; // Changed to protected
+		}
+	}
 }
