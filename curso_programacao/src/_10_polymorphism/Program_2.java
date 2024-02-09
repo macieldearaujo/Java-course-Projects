83b3b6763efb2c5f8219ea70e8e0071c00e45572
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-08 23:52:14 -0300

    program2 - upcasting and downcasting

---
+++ curso_programacao/src/_10_polymorphism/Program_2.java
@@ -0,0 +1,37 @@
+package _10_polymorphism;
+
+import entities.Account;
+import entities.BussinessAccount;
+import entities.SavingAccount;
+
+public class Program_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account acc = new Account(1500, "Alex", 0.0);
+		BussinessAccount bacc = new BussinessAccount(1501, "Maria", 0.0, 500.0);
+		
+		// UPCASTING
+		Account acc1 = bacc;
+		Account acc2 = new BussinessAccount(1502, "Maria", 0.0, 500.0);
+		Account acc3 = new SavingAccount(1505, "Alexandre", 0.0, 0.01);
+		
+		// DOWNCASTING
+		BussinessAccount acc4 = (BussinessAccount)acc2; // In case you want use a function that is in a subclass in a superclass you have to downcasting
+		acc4.loan(1500.0); // Just like this
+		
+		if(acc3 instanceof BussinessAccount) {
+			BussinessAccount acc5 = (BussinessAccount)acc3;
+			acc5.loan(200.00);
+			System.out.println("Loan!");
+		}
+		
+		if(acc3 instanceof SavingAccount) {
+			SavingAccount acc5 = (SavingAccount) acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+	}
+
+}
