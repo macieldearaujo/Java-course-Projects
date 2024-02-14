a8e1c076ee83960555ffca291c4aa2b9cbcd0ffb
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-14 18:13:42 -0300

    program6 - abstract class (addapting the code for abstract method)

---
+++ curso_programacao/src/entities/Abst_Account.java
@@ -0,0 +1,46 @@
+package entities;
+
+public abstract class Abst_Account {
+	
+	private int account;
+	private String holder;
+	protected double balance;  ///  ******************
+	
+	public Abst_Account() {
+	}
+	
+	public Abst_Account(int account, String holder, double balance) {
+		this.account = account;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public int getAccount() {
+		return account;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void deposit(double value) {
+		this.balance += value;
+	}
+	
+	public void withdraw(double value) {
+		this.balance -= (value + 5);
+	}
+	
+	public String toString() {
+		return "Account: " + account + ", Holder: " + holder + ", Balance:  $ " + String.format("%.2f", balance);
+	}
+	
+}
