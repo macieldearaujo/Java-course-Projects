f7d48b64f93a15963946d2df768dbd0385b76716
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-14 22:29:02 -0300

    ex002 - abstract - legalPerson class

---
+++ curso_programacao/src/entities/LegalPerson.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class LegalPerson extends TaxPayer {
+
+	private int employeeNumber;
+
+	public LegalPerson() {
+		super();
+	}
+	
+	public LegalPerson(String name, Double annualSalary, int employeeNumber) {
+		super(name, annualSalary);
+		this.employeeNumber = employeeNumber;
+	}
+	
+	public int getEmployeeNumber() {
+		return employeeNumber;
+	}
+
+	public void setEmployeeNumber(int employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+
+	@Override
+	public Double tax() {
+		if(employeeNumber < 10) {
+			return getAnnualSalary() * 0.16;
+		} else {
+			return getAnnualSalary() * 0.14;
+		}
+	}
+	
+}
