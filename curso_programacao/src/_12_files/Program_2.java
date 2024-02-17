54fd8dda836946b7331be8d3e3f5dbb3df29b1f2
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-17 12:41:50 -0300

    try block with resources

--- curso_programacao/src/_12_files/Program_2.java
+++ curso_programacao/src/_12_files/Program_2.java
@@ -10,8 +10,13 @@ public class Program_2 {
 		// TODO Auto-generated method stub
 	
 		String path = "C:\\temp\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
 			String line = br.readLine();
 			
 			while(line != null) {
@@ -21,6 +26,19 @@ public class Program_2 {
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+				if(fr != null) {
+					fr.close();
+				}
+			} 
+			catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
 		}
 		
 	}
