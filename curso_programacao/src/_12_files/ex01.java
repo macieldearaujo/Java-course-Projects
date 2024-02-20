35c5d58e30fdf82379ade01e7bfd703d2f6f6061
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-20 01:20:09 -0300

    saving changes

--- curso_programacao/src/_12_files/ex01.java
+++ curso_programacao/src/_12_files/ex01.java
@@ -2,40 +2,34 @@ package _12_files;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import model.entities.Product;
 
 public class ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
 		List<Product> products = new ArrayList<>();
+		
+		System.out.println("Enter a path: ");
+		String sourceFileStr = sc.nextLine();
 
-		try (BufferedReader br = new BufferedReader(new FileReader("C:\\temp\\testing_files\\in.csv"));
-			BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\temp\\testing_files\\out\\summary.csv"))) {			
-
-			String line = br.readLine();
-
-			while (line != null) {
-				String[] words = line.split(",");
-				Product prod = new Product(words[0], Double.parseDouble(words[1]), Integer.parseInt(words[2]));
-				products.add(prod);
-				
-				line = br.readLine();
-			}
-			
-			for(Product p : products) {
-				bw.append(p.getName() + "," + p.totalValueinStock());
-				bw.newLine();
-			}
-			
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
+		
+		boolean sucess = new File(sourceFolderStr + "/out").mkdir();
+		
+		System.out.println("Folder created: " + sucess);
+		
+		sc.close();
 	}
 }
