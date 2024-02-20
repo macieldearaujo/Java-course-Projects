d50e379fdfb5e2f642d729c80d9161d2f447e78b
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-20 01:20:20 -0300

    bin

--- curso_programacao/src/_12_files/Program_2.java
+++ curso_programacao/src/_12_files/Program_2.java
@@ -3,26 +3,34 @@ package _12_files;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Product;
 
 public class Program_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		String path = "C:\\temp\\in.txt";
+		String path = "C:\\temp\\testing_files\\in.csv";
+		List<Product> products = new ArrayList<>();
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
 			String line = br.readLine();
 			
 			while(line != null) {
-				System.out.println(line);
+				String[] words = line.split(",");
+				for(String a : words) {
+					Product prod = new Product(words[0], Double.parseDouble(words[1]), Integer.parseInt(words[2]));
+					products.add(prod);
+				}
+				
 				line = br.readLine();
-			}
-		}
-		catch (IOException e) {
+			}			
+		} catch(IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		
 	}
-	
 }
--- curso_programacao/src/model/entities/Product.java
+++ curso_programacao/src/model/entities/Product.java
@@ -6,6 +6,35 @@ public class Product {
 	public double price;
 	public int quantity;
 	
+	public Product() {
+	}
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
 	public double totalValueinStock() {
 		return quantity * price;
 	}
