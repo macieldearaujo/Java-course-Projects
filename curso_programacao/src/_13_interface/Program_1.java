f3ceee75912a23ce44c277361581a172901589fc
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-22 18:14:31 -0300

    program1 - interface - main program

---
+++ curso_programacao/src/_13_interface/Program_1.java
@@ -0,0 +1,22 @@
+package _13_interface;
+
+import model.entities.Abst_Forma;
+import model.entities.Circulo;
+import model.entities.Retangulo;
+import model.enums.Color;
+
+public class Program_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Abst_Forma s1 = new Circulo(Color.BLACK, 2.0);
+		Abst_Forma s2 = new Retangulo(Color.BLUE, 3.0, 4.0);
+		
+		System.out.println("Circle color: " + s1.getColor());
+		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
+		System.out.println("Rectangle color: " + s2.getColor());
+		System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
+		
+	}
+}
