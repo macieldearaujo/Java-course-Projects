5fb96e53e3d2a23c52c403762db404195a81a3b2
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-24 15:05:42 -0300

    program_4 - default method - main program

---
+++ curso_programacao/src/_13_interface/Program_4.java
@@ -0,0 +1,31 @@
+package _13_interface;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.services.InterestService;
+import model.services.UsaInterestService;
+
+public class Program_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantia: ");
+		double value = sc.nextDouble();
+		System.out.print("Meses: ");
+		int months = sc.nextInt();
+		
+		InterestService is = new UsaInterestService(1.0);
+		double payment = is.payment(value, months);
+		
+		System.out.println("Payment after " + months + " months.");		
+		System.out.println(String.format("%.2f", payment));
+		
+		sc.close();
+	}
+
+}
