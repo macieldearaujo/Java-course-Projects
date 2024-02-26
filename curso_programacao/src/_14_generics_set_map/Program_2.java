06d9ed8754b420b143ab9999e9bbe8b3d59a553a
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-26 18:16:07 -0300

    program2 - bounded generics

---
+++ curso_programacao/src/_14_generics_set_map/Program_2.java
@@ -0,0 +1,40 @@
+package _14_generics_set_map;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import model.entities.ProductGenerics;
+import model.services.CalculationService;
+
+public class Program_2 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		String path = "C:\\\\temp\\\\testing_files\\\\generics.csv";
+		List<ProductGenerics> list = new ArrayList<>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			String line = br.readLine();
+			
+			while(line != null) {
+				String[] fields = line.split(",");
+				ProductGenerics prod = new ProductGenerics(fields[0], Double.parseDouble(fields[1]));
+				list.add(prod);
+				
+				line = br.readLine();
+			}
+			
+			System.out.println("Most expensive:");
+			System.out.println(CalculationService.max(list));
+			
+		} catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
