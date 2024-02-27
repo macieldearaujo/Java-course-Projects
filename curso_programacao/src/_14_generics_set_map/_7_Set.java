4160ea9a2c450a5f66355c4fe42a4a557349f272
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-27 19:20:28 -0300

    program7 - set

---
+++ curso_programacao/src/_14_generics_set_map/_7_Set.java
@@ -0,0 +1,23 @@
+package _14_generics_set_map;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _7_Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Set<String> set = new HashSet<>(); // HashSet, TreeSet and LinkedSet
+		
+		set.add("Tv");
+		set.add("Notebook");
+		set.add("Tablet");
+		
+		System.out.println(set.contains("Notebook"));
+		
+		for(String p : set) {
+			System.out.println(p);
+		}
+	}
+}
