0361f5d0074f18862f0d9d476cf63db233c3c4bf
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-27 18:41:13 -0300

    program6 - hashCode and Equals

---
+++ curso_programacao/src/_14_generics_set_map/_6_HashCode_Equals.java
@@ -0,0 +1,24 @@
+package _14_generics_set_map;
+
+import model.entities.Cliente;
+
+public class _6_HashCode_Equals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cliente c1 = new Cliente("Maria", "maria@gmail.com");
+		Cliente c2 = new Cliente("Alex", "alex@gmail.com");
+		
+		String s1 = "Test";
+		String s2 = "Test";
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		System.out.println(c1 == c2); // Instance
+		System.out.println(s1 == s2); // Primitive type
+		
+	}
+
+}
