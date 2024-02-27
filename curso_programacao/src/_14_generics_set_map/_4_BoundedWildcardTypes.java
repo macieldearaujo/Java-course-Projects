5a6e92cc355d344824452c03a847a4b6e5aa9a2b
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-27 13:29:55 -0300

    program4 - BoundedWildcardTypes

---
+++ curso_programacao/src/_14_generics_set_map/_4_BoundedWildcardTypes.java
@@ -0,0 +1,29 @@
+package _14_generics_set_map;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Circulo2;
+import model.entities.Formato;
+import model.entities.Retangulo2;
+
+public class _4_BoundedWildcardTypes {
+
+	public static void main(String[] args) {
+
+		List<Formato> myShapes = new ArrayList<>();
+		myShapes.add(new Retangulo2(3.0, 2.0));
+		myShapes.add(new Circulo2(2.0));
+		
+	}
+	
+	public static double totalArea(List<? extends Formato> list) {
+		Double sum = 0.0;
+		
+		for(Formato value : list) {
+			sum += value.area();
+		}
+		return sum;
+	}
+
+}
