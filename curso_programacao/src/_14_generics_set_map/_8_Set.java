374b4d5845a52e2e9b5327d2494537575330d610
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-02-28 08:51:32 -0300

    program8 - set (comparative)

---
+++ curso_programacao/src/_14_generics_set_map/_8_Set.java
@@ -0,0 +1,22 @@
+package _14_generics_set_map;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import model.entities.Product;
+
+public class _8_Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Set<Product> set = new HashSet<>();
+		set.add(new Product("Tv", 2000.00, 2));
+		set.add(new Product("Notebook", 1200.00, 1)); // contains hashCode and equals
+		set.add(new Product("Tablet", 1600, 4));
+		
+		Product p1 = new Product("Notebook", 1200.00, 1);
+		
+		System.out.println(set.contains(p1));
+	}
+}
