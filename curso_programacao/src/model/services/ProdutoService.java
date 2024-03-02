157f65036931c8383616c17a3e7f1ea38aa6956e
douglasmcl <137844230+macieldearaujo@users.noreply.github.com>
2024-03-02 00:59:03 -0300

    program5 - adding a function to the parameter of another function

---
+++ curso_programacao/src/model/services/ProdutoService.java
@@ -0,0 +1,19 @@
+package model.services;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import model.entities.Produto;
+
+public class ProdutoService {
+
+	public Double filteredSum(List<Produto> list, Predicate<Produto> criteria) {
+		double sum = 0.00;
+		for(Produto p : list) {
+			if(criteria.test(p)) {
+				sum += p.getPrice();
+			}
+		}
+		return sum;
+	}
+}
